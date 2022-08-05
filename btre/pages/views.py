1eb1d8f9d8664ddf5f7d480876a53bf0356a645c
Sprint <tamkah@hotmail.com>
2022-08-05 11:00:29 +0800

    add newer files

--- btre/pages/views.py
+++ btre/pages/views.py
@@ -1,35 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
-from listings.choices import price_choices, state_choices, bedroom_choices
-
-from listings.models import Realtors
+from django.shortcuts import render
 
 # Create your views here.
+
+# import dictionary for choices
+from listings.choices import price_choices, state_choices, bedroom_choices
+
 # create a function for the index link to urls API route
+from listings.models import Listing
 
+# about page is mainly realtor=, so import realtor model
+from realtors.models import Realtor
 
+# create a function for the index link to urls API route
 def index(request):
-    # shown3nlisting only
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-    # add choices into the subject
+    # show 3listing only
+    listings = Listing.objecs.order_by('-list_date').filter(is_published=True)[:3]
+    # add choices into the object
     context = {
-        'listings': listings,
+         #'listings': listings,
         'state_choices': state_choices,
         'price_choices': price_choices,
-        'bedroom_choices': bedroom_choices
+        'bedroom_choices': bedroom_choices,
     }
-     return render(request, "pages/index.html", context)
-
-def about(request):
-    # get all realtors
-    realtors = Realtor.objects.order_by(-hire_date),
-    # get MVP, which is only one
-    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
-    context = {
-        'realtors': realtors,
-        'mvp_realtors': mvp_realtors
-    }
-
-    return render(request, "pages/about.html", context)
-
+    return render(request, "pages/index.html", context)
